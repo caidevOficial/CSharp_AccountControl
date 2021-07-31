dce78832d294a2e670d97a72e906755f6cc80cde
CaidevOficial <falcone.facu@gmail.com>
2021-07-31 11:43:38 -0300

    Added an interface in DAO to handle CRUDs operations

--- Account_Control/Account_Control.Logic/Account_Control.Logic.Classes/Account_Control.Logic.Classes.DAOLayer/ConnectionDAO.cs
+++ Account_Control/Account_Control.Logic/Account_Control.Logic.Classes/Account_Control.Logic.Classes.DAOLayer/ConnectionDAO.cs
@@ -29,7 +29,7 @@ using System.Data.SqlClient;
 using Models;
 
 namespace DAOLayer {
-    public class ConnectionDAO : DAOAbstract {
+    public class ConnectionDAO : DAOAbstract, IDAOCRUD<Ticket>, IDAOCRUD<Payment> {
 
         #region Attributes
 
@@ -42,10 +42,6 @@ namespace DAOLayer {
 
         #region Methods
 
-        public static String GetTimestamp(DateTime value) {
-            return value.ToString("yyyyMMddHHmmssffff");
-        }
-
         /// <summary>
         /// Creates an object and insert it into the DB.
         /// </summary>
@@ -57,9 +53,9 @@ namespace DAOLayer {
             if (typeEntity is EntityType.Customer) {
                 success = this.CreateCustomer((Customer)workItem);
             } else if (typeEntity is EntityType.Ticket) {
-                success = this.CreateTicket((Ticket)workItem);
+                success = ((IDAOCRUD<Ticket>)this).CreateObject((Ticket)workItem);
             } else {
-                success = this.CreatePayment((Payment)workItem);
+                success = this.CreateObject((Payment)workItem);
             }
 
             return success;
@@ -84,7 +80,7 @@ namespace DAOLayer {
                     int rows = ConnectionDAO.MyCommand.ExecuteNonQuery();
                     success = true;
                 } else {
-                    throw new Exception();
+                    throw new ArgumentNullException();
                 }
             } catch (Exception ex) {
                 throw new Exception($"Error {CREATE_VERB} {objectName}", ex);
@@ -96,6 +92,10 @@ namespace DAOLayer {
             return success;
         }
 
+        /// <summary>
+        /// Gets a list of all tickets of the DB.
+        /// </summary>
+        /// <returns>A list of all tickets of the DB.</returns>
         public List<Ticket> ReadAllTickets() {
             List<Ticket> tickets = new List<Ticket>();
             Ticket actualTicket;
@@ -119,12 +119,192 @@ namespace DAOLayer {
             return tickets;
         }
 
-        public static void UpdateTicket() {
+        /// <summary>
+        /// Reads all tickets of customer id 'idCustomer'.
+        /// </summary>
+        /// <param name="idCustomer">ID of the customer to search it's tickets.</param>
+        /// <returns>A list of tickets of the customer.</returns>
+        public List<Ticket> ReadAllTicketsByCustomer(short idCustomer) {
 
+            List<Ticket> payments = new List<Ticket>();
+            Ticket actualTicket;
+            ConnectionDAO.MyCommand.CommandText = "Select * from Tickets WHERE Id_Customer = @idCustomer";
+            ConnectionDAO.MyCommand.Parameters.AddWithValue("@IdCustomer", idCustomer);
+            ConnectionDAO.MyConection.Open();
+            SqlDataReader myReader = ConnectionDAO.MyCommand.ExecuteReader();
+            DataTable myDT = new DataTable();
+            myDT.Load(myReader);
+            try {
+                foreach (DataRow item in myDT.Rows) {
+                    float.TryParse(item["Amount"].ToString(), out float amount);
+                    actualTicket = new Ticket(Convert.ToInt16(item["id"]), Convert.ToDateTime(item["Date"]), amount, Convert.ToInt16(item["Id_Customer"]));
+                    payments.Add(actualTicket);
+                }
+            } catch (Exception ex) {
+                throw new Exception($"Error {READ_VERB} tickets.", ex);
+            } finally {
+                ConnectionDAO.MyCommand.Parameters.Clear();
+                ConnectionDAO.MyConection.Close();
+            }
+
+            return payments;
+        }
+
+        /// <summary>
+        /// Reads all Tickets of an specific date (DateTime).
+        /// </summary>
+        /// <param name="ticketDate">Date in DateTime format to search the Tickets.</param>
+        /// <returns>A list of Tickets of an specific date.</returns>
+        public List<Ticket> ReadAllTicketsByDate(DateTime ticketDate) {
+
+            List<Ticket> tickets = new List<Ticket>();
+            Ticket actualTicket;
+            ConnectionDAO.MyCommand.CommandText = "Select * from Tickets WHERE Date = @ticketDate";
+            ConnectionDAO.MyCommand.Parameters.AddWithValue("@ticketDate", ticketDate);
+            ConnectionDAO.MyConection.Open();
+            SqlDataReader myReader = ConnectionDAO.MyCommand.ExecuteReader();
+            DataTable myDT = new DataTable();
+            myDT.Load(myReader);
+            try {
+                foreach (DataRow item in myDT.Rows) {
+                    float.TryParse(item["Amount"].ToString(), out float amount);
+                    actualTicket = new Ticket(Convert.ToInt16(item["id"]), Convert.ToDateTime(item["Date"]), amount, Convert.ToInt16(item["Id_Customer"]));
+                    tickets.Add(actualTicket);
+                }
+            } catch (Exception ex) {
+                throw new Exception($"Error {READ_VERB} payments.", ex);
+            } finally {
+                ConnectionDAO.MyCommand.Parameters.Clear();
+                ConnectionDAO.MyConection.Close();
+            }
+
+            return tickets;
+        }
+
+        public bool UpdateTicket() {
+            bool success = false;
+
+            return success;
         }
 
         public static void DeleteTicket() {
 
+        }
+
+        /// <summary>
+        /// Creates a ticket in the DB.
+        /// </summary>
+        /// <param name="myObject">Ticket to be inserted into the db.</param>
+        public bool CreateObject(Ticket myObject) {
+            bool success = false;
+            string objectName = myObject.GetType().Name;
+            try {
+                if (!(myObject is null)) {
+                    ConnectionDAO.MyConection.Open();
+                    ConnectionDAO.MyCommand.CommandText = $"INSERT INTO Tickets Values(@Date, @id_Customer, @Amount);";
+                    ConnectionDAO.MyCommand.Parameters.AddWithValue("@Date", myObject.WorkItemDate);
+                    ConnectionDAO.MyCommand.Parameters.AddWithValue("@id_Customer", myObject.WorkItemIDCustomer);
+                    ConnectionDAO.MyCommand.Parameters.AddWithValue("@Amount", Math.Round(myObject.WorkItemAmount, 2));
+                    int rows = ConnectionDAO.MyCommand.ExecuteNonQuery();
+                    success = true;
+                } else {
+                    throw new ArgumentNullException();
+                }
+            } catch (Exception ex) {
+                throw new Exception($"Error {CREATE_VERB} {objectName}", ex);
+            } finally {
+                ConnectionDAO.MyCommand.Parameters.Clear();
+                ConnectionDAO.MyConection.Close();
+            }
+
+            return success;
+        }
+
+        /// <summary>
+        /// Gets a list of all tickets of the DB.
+        /// </summary>
+        /// <returns>A list of all tickets of the DB.</returns>
+        List<Ticket> IDAOCRUD<Ticket>.ReadAllObjects() {
+            List<Ticket> tickets = new List<Ticket>();
+            Ticket actualTicket;
+            ConnectionDAO.MyCommand.CommandText = "Select * from Tickets";
+            ConnectionDAO.MyConection.Open();
+            SqlDataReader myReader = ConnectionDAO.MyCommand.ExecuteReader();
+            DataTable myDT = new DataTable();
+            myDT.Load(myReader);
+            try {
+                foreach (DataRow item in myDT.Rows) {
+                    float.TryParse(item["Amount"].ToString(), out float amount);
+                    actualTicket = new Ticket(Convert.ToInt16(item["id"]), Convert.ToDateTime(item["Date"]), amount, Convert.ToInt16(item["Id_Customer"]));
+                    tickets.Add(actualTicket);
+                }
+            } catch (Exception ex) {
+                throw new Exception($"Error {READ_VERB} tickets.", ex);
+            } finally {
+                ConnectionDAO.MyConection.Close();
+            }
+
+            return tickets;
+        }
+
+        /// <summary>
+        /// Reads all tickets of customer id 'idCustomer'.
+        /// </summary>
+        /// <param name="idCustomer">ID of the customer to search it's tickets.</param>
+        /// <returns>A list of tickets of the customer.</returns>
+        List<Ticket> IDAOCRUD<Ticket>.ReadAllObjectsByCustomer(short idCustomer) {
+            List<Ticket> payments = new List<Ticket>();
+            Ticket actualTicket;
+            ConnectionDAO.MyCommand.CommandText = "Select * from Tickets WHERE Id_Customer = @idCustomer";
+            ConnectionDAO.MyCommand.Parameters.AddWithValue("@IdCustomer", idCustomer);
+            ConnectionDAO.MyConection.Open();
+            SqlDataReader myReader = ConnectionDAO.MyCommand.ExecuteReader();
+            DataTable myDT = new DataTable();
+            myDT.Load(myReader);
+            try {
+                foreach (DataRow item in myDT.Rows) {
+                    float.TryParse(item["Amount"].ToString(), out float amount);
+                    actualTicket = new Ticket(Convert.ToInt16(item["id"]), Convert.ToDateTime(item["Date"]), amount, Convert.ToInt16(item["Id_Customer"]));
+                    payments.Add(actualTicket);
+                }
+            } catch (Exception ex) {
+                throw new Exception($"Error {READ_VERB} tickets.", ex);
+            } finally {
+                ConnectionDAO.MyCommand.Parameters.Clear();
+                ConnectionDAO.MyConection.Close();
+            }
+
+            return payments;
+        }
+
+        /// <summary>
+        /// Reads all Tickets of an specific date (DateTime).
+        /// </summary>
+        /// <param name="ticketDate">Date in DateTime format to search the Tickets.</param>
+        /// <returns>A list of Tickets of an specific date.</returns>
+        List<Ticket> IDAOCRUD<Ticket>.ReadAllObjectsByDate(DateTime ticketDate) {
+            List<Ticket> tickets = new List<Ticket>();
+            Ticket actualTicket;
+            ConnectionDAO.MyCommand.CommandText = "Select * from Tickets WHERE Date = @ticketDate";
+            ConnectionDAO.MyCommand.Parameters.AddWithValue("@ticketDate", ticketDate);
+            ConnectionDAO.MyConection.Open();
+            SqlDataReader myReader = ConnectionDAO.MyCommand.ExecuteReader();
+            DataTable myDT = new DataTable();
+            myDT.Load(myReader);
+            try {
+                foreach (DataRow item in myDT.Rows) {
+                    float.TryParse(item["Amount"].ToString(), out float amount);
+                    actualTicket = new Ticket(Convert.ToInt16(item["id"]), Convert.ToDateTime(item["Date"]), amount, Convert.ToInt16(item["Id_Customer"]));
+                    tickets.Add(actualTicket);
+                }
+            } catch (Exception ex) {
+                throw new Exception($"Error {READ_VERB} payments.", ex);
+            } finally {
+                ConnectionDAO.MyCommand.Parameters.Clear();
+                ConnectionDAO.MyConection.Close();
+            }
+
+            return tickets;
         }
 
         #endregion
@@ -158,6 +338,37 @@ namespace DAOLayer {
             return success;
         }
 
+        /// <summary>
+        /// Creates a Payment in the DB.
+        /// </summary>
+        /// <param name="myObject">Payment to be inserted into the db.</param>
+        public bool CreateObject(Payment myObject) {
+            bool success = false;
+            string objectName = myObject.GetType().Name;
+            try {
+                if (!(myObject is null)) {
+                    ConnectionDAO.MyConection.Open();
+                    ConnectionDAO.MyCommand.CommandText = $"INSERT INTO Payments Values(@Date,@IdCustomer,@Amount);";
+                    ConnectionDAO.MyCommand.Parameters.AddWithValue("@Date", myObject.WorkItemDate);
+                    ConnectionDAO.MyCommand.Parameters.AddWithValue("@Amount", Math.Round(myObject.WorkItemAmount, 2));
+                    ConnectionDAO.MyCommand.Parameters.AddWithValue("@IdCustomer", myObject.WorkItemIDCustomer);
+                    int rows = ConnectionDAO.MyCommand.ExecuteNonQuery();
+                    success = true;
+                }
+            } catch (Exception ex) {
+                throw new Exception($"Error {CREATE_VERB} {objectName}.", ex);
+            } finally {
+                ConnectionDAO.MyCommand.Parameters.Clear();
+                ConnectionDAO.MyConection.Close();
+            }
+
+            return success;
+        }
+
+        /// <summary>
+        /// Gets a list of all Payments of the DB.
+        /// </summary>
+        /// <returns>A list of all Payments of the DB.</returns>
         public static List<Payment> ReadAllPayments() {
 
             List<Payment> payments = new List<Payment>();
@@ -177,7 +388,34 @@ namespace DAOLayer {
             return payments;
         }
 
-        public static List<Payment> ReadAllPaymentsByCustomer(short idCustomer) {
+        /// <summary>
+        /// Gets a list of all Payments of the DB.
+        /// </summary>
+        /// <returns>A list of all Payments of the DB.</returns>
+        public List<Payment> ReadAllObjects() {
+            List<Payment> payments = new List<Payment>();
+            Payment actualPayment;
+            ConnectionDAO.MyCommand.CommandText = "Select * from Payments";
+            ConnectionDAO.MyConection.Open();
+            SqlDataReader myReader = ConnectionDAO.MyCommand.ExecuteReader();
+            DataTable myDT = new DataTable();
+            myDT.Load(myReader);
+            foreach (DataRow item in myDT.Rows) {
+                float.TryParse(item["Amount"].ToString(), out float amount);
+                actualPayment = new Payment(Convert.ToInt16(item["id"]), Convert.ToDateTime(item["Date"]), amount, Convert.ToInt16(item["Id_Customer"]));
+                payments.Add(actualPayment);
+            }
+            ConnectionDAO.MyConection.Close();
+
+            return payments;
+        }
+
+        /// <summary>
+        /// Reads all Payment of customer id 'idCustomer'.
+        /// </summary>
+        /// <param name="idCustomer">ID of the customer to search it's Payments.</param>
+        /// <returns>A list of Payments of the customer.</returns>
+        public List<Payment> ReadAllPaymentsByCustomer(short idCustomer) {
 
             List<Payment> payments = new List<Payment>();
             Payment actualPayment;
@@ -203,8 +441,73 @@ namespace DAOLayer {
             return payments;
         }
 
+        /// <summary>
+        /// Reads all Payment of customer id 'idCustomer'.
+        /// </summary>
+        /// <param name="idCustomer">ID of the customer to search it's Payments.</param>
+        /// <returns>A list of Payments of the customer.</returns>
+        public List<Payment> ReadAllObjectsByCustomer(short idCustomer) {
+            List<Payment> payments = new List<Payment>();
+            Payment actualPayment;
+            ConnectionDAO.MyCommand.CommandText = "Select * from Payments WHERE Id_Customer = @idCustomer";
+            ConnectionDAO.MyCommand.Parameters.AddWithValue("@IdCustomer", idCustomer);
+            ConnectionDAO.MyConection.Open();
+            SqlDataReader myReader = ConnectionDAO.MyCommand.ExecuteReader();
+            DataTable myDT = new DataTable();
+            myDT.Load(myReader);
+            try {
+                foreach (DataRow item in myDT.Rows) {
+                    float.TryParse(item["Amount"].ToString(), out float amount);
+                    actualPayment = new Payment(Convert.ToInt16(item["id"]), Convert.ToDateTime(item["Date"]), amount, Convert.ToInt16(item["Id_Customer"]));
+                    payments.Add(actualPayment);
+                }
+            } catch (Exception ex) {
+                throw new Exception($"Error {READ_VERB} payments.", ex);
+            } finally {
+                ConnectionDAO.MyCommand.Parameters.Clear();
+                ConnectionDAO.MyConection.Close();
+            }
+
+            return payments;
+        }
+
+        /// <summary>
+        /// Reads all Payment of an specific date (DateTime).
+        /// </summary>
+        /// <param name="paymentDate">Date in DateTime format to search the payments.</param>
+        /// <returns>A list of payments of an specific date.</returns>
         public static List<Payment> ReadAllPaymentsByDate(DateTime paymentDate) {
 
+            List<Payment> payments = new List<Payment>();
+            Payment actualPayment;
+            ConnectionDAO.MyCommand.CommandText = "Select * from Payments WHERE Date = @paymentDate";
+            ConnectionDAO.MyCommand.Parameters.AddWithValue("@paymentDate", paymentDate);
+            ConnectionDAO.MyConection.Open();
+            SqlDataReader myReader = ConnectionDAO.MyCommand.ExecuteReader();
+            DataTable myDT = new DataTable();
+            myDT.Load(myReader);
+            try {
+                foreach (DataRow item in myDT.Rows) {
+                    float.TryParse(item["Amount"].ToString(), out float amount);
+                    actualPayment = new Payment(Convert.ToInt16(item["id"]), Convert.ToDateTime(item["Date"]), amount, Convert.ToInt16(item["Id_Customer"]));
+                    payments.Add(actualPayment);
+                }
+            } catch (Exception ex) {
+                throw new Exception($"Error {READ_VERB} payments.", ex);
+            } finally {
+                ConnectionDAO.MyCommand.Parameters.Clear();
+                ConnectionDAO.MyConection.Close();
+            }
+
+            return payments;
+        }
+
+        /// <summary>
+        /// Reads all Payment of an specific date (DateTime).
+        /// </summary>
+        /// <param name="paymentDate">Date in DateTime format to search the payments.</param>
+        /// <returns>A list of payments of an specific date.</returns>
+        public List<Payment> ReadAllObjectsByDate(DateTime paymentDate) {
             List<Payment> payments = new List<Payment>();
             Payment actualPayment;
             ConnectionDAO.MyCommand.CommandText = "Select * from Payments WHERE Date = @paymentDate";
