ca700e0dbce474eb29df335e7b52006f19ef96de
CaidevOficial <falcone.facu@gmail.com>
2021-07-31 02:43:25 -0300

    Updated GUI Logic

---
+++ Account_Control/Account_Control.GUI/Account_Control.GUI.Tickets/frmAddTicket.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using DAOLayer;
+using Models;
+
+namespace Account_Control {
+    public partial class frmAddTicket : Form {
+
+        private List<Customer> customers;
+        private Ticket myTicket;
+        private ConnectionDAO daoManager;
+        public frmAddTicket() {
+            InitializeComponent();
+            daoManager = new ConnectionDAO();
+        }
+
+        private void frmAddTicket_Load(object sender, EventArgs e) {
+            customers = ConnectionDAO.ReadAllCustomers();
+            cmbCustomer.DataSource = customers;
+        }
+
+        private void btnCreateTicket_Click(object sender, EventArgs e) {
+            DateTime date = dtpTime.Value;
+            float.TryParse(txtAmount.Text.Replace('.', ','), out float amount);
+            myTicket = new Ticket(date, amount, ((Customer)cmbCustomer.SelectedItem).ID);
+            if (daoManager.Create(myTicket, EntityType.Ticket)) {
+                MessageBox.Show("Remito Creado con exito.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Dispose();
+            }
+        }
+    }
+}
